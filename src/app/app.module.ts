488257c98240c3d9ef4acbf200712b07ec73c084
Leandro Caetano <lcaetano30@gmail.com>
2022-04-01 19:02:35 -0300

    Modelagem do template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia.component';
 import {FormsModule} from "@angular/forms";
+import { RoutesComponent } from './component/routes/routes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
+    RoutesComponent
   ],
   imports: [
     BrowserModule,
